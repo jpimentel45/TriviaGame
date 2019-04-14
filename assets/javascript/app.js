e06c9f1193c13e7be74de96094d35a435bc6d29a
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-04-14 15:05:33 -0700

    Fixed bug on image function, added a bit more css, more properties to trivia object, and more gifs to image folder.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
         },
         {
             question: "During sleepovers, Mystery Date allowed girls to date which sexy actor?",
-            choices: ["Port", "James Franco", "Paul Rudd", "Chris Evans, Mr. America"],
+            choices: ["Portabella Mushroom", "James Franco", "Paul Rudd", "Chris Evans, Mr. America"],
             rightChoice: 3,
             image: "assets/images/chris-evans.gif",
             background: "<img src='assets/images/90cart.jpg'>"
@@ -48,15 +48,50 @@ $(document).ready(function () {
             rightChoice: 3,
             image: "assets/images/zig-a-zig-ha.gif",
             background: "<img src='assets/images/90back.jpg'>"
+        },
+        {
+            question: "Which child sang Survivor to bring Jesus into our hearts?",
+            choices: ["Striperella👩‍🎤", "Dusty Diamondz💎", "Destiny's Child 😜", "Jeniffer Lopez🧟‍"],
+            rightChoice: 2,
+            image: "assets/images/Destiny.gif",
+            background: "<img src='assets/images/90cart.jpg'>"
+        },
+        {
+            question: "Which device allowed us to end a call dramatically after an argument?",
+            choices: ["Razr💅", "La Chancla🙌", "Low Battery📵", "The telephone👵"],
+            rightChoice: 0,
+            image: "assets/images/Razr.gif",
+            background: "<img src='assets/images/90back.jpg'>"
+        },
+        {
+            question: "Jessica Simpson released what type of edible product?",
+            choices: ["Lubricant", "Lingerie", "Lipstick", "Perfume"],
+            rightChoice: 3,
+            image: "assets/images/Jessica-Simpson.gif",
+            background: "<img src='assets/images/90cart.jpg'>"
+        },
+        {
+            question: "Which popular device was most confiscated during the late 2000's?",
+            choices: ["iPod Classic", "Hot Cheetos", "iPhone X", "Lanterns"],
+            rightChoice: 0,
+            image: "assets/images/iPod-Classic.gif",
+            background: "<img src='assets/images/90back.jpg'>"
+        },
+        {
+            question: "What is the strongest material on Earth?",
+            choices: ["Diamonds", "Aubrey Plaza", "Unobtainium", "Nokia Phone"],
+            rightChoice: 3,
+            image: "assets/images/Nokia.gif",
+            background: "<img src='assets/images/90cart.jpg'>"
         }
     ];
+
     var rightAnswer = 0;
     var wrongAnswer = 0;
     var unansweredCount = 0;
-    var time = 30;
+    var time = 15;
     var intervalId;
     var userSelection = "";
-    var selected = false;
     var running = false;
     var totalCount = trivia.length;
     var chosenOne;
@@ -64,18 +99,17 @@ $(document).ready(function () {
     var newArray = [];
     var placeHolder = [];
 
+
     //hide resetBtn until called
     $("#resetBtn").hide();
-    //click startBtn button to start game
-    $("#startBtn").on("click", function () {
+    //click startBtn button to startame & hide btn
+    $("#startBtn").click(function () {
         $(this).hide();
-        //$(".container").preppend("<img src=" + chosenOne.background + ">");
         displayTrivia();
         runTime();
         for (var i = 0; i < trivia.length; i++) {
             placeHolder.push(trivia[i]);
-        };
-
+        }
     })
     //time: run
     function runTime() {
@@ -89,13 +123,12 @@ $(document).ready(function () {
         $("#timeLeft").html("<h4>👻 Madonna, we're running out of time 👻 " + time + " 👀</h4>");
         time--;
 
-        //stop time if reach 0
+        //stop time===0
         if (time === 0) {
             unansweredCount++;
             stop();
-            //clearbck();
-            $("#choicesDiv").html("<p>Oh no! You ran out of time 😂. The correct choice is: " + chosenOne.choices[chosenOne.rightChoice] + "</p>");
-            hideimage();
+            $("#choicesDiv").html("<p>You ran out of time 😂. The correct choice is: " + chosenOne.choices[chosenOne.rightChoice] + "</p>");
+            hideImage();
         }
     }
 
@@ -104,19 +137,13 @@ $(document).ready(function () {
         running = false;
         clearInterval(intervalId);
     }
-    //function newBackground() {
-    // $(".container").preppend("<img src=" + chosenOne.background + ">");
-    //  newArray.push(chosenOne);
-    // trivia.splice(triviaRand, 1);
-    // }
-    //display question and loop though and display possible answers
+
+    //display trivia question, loop,  and display choices
     function displayTrivia() {
         //generate random triviaRand in array
         triviaRand = Math.floor(Math.random() * trivia.length);
-        //console.log(triviaRand);
         chosenOne = trivia[triviaRand];
-        console.log(chosenOne);
-        //$(".container").prepend("<div class='newBackground'>" + chosenOne.background + "</div>");
+        //console.log(chosenOne);
 
         $("#questionDiv").html("<h2>" + chosenOne.question + "</h2>");
         for (var i = 0; i < chosenOne.choices.length; i++) {
@@ -126,93 +153,84 @@ $(document).ready(function () {
             //assign array position to it so can check rightChoice
             newUserChoice.attr("userChoices", i);
             $("#choicesDiv").append(newUserChoice);
-            //try prepend to add a div containing background img tag
-            //and add background property into object array for each question
-            //use css & z-index to position behind actual container	
-            //$(".container").prepend(chosenOne.background);
-            //console.log(chosenOne.background);
-
-            //click function to select rightChoice
-            $(".answerChoices").click(function () {
-                //parseInt() function parses a string argument and returns an integer of the specified radix
-                //locate array based on userChoice
-                userSelection = parseInt($(this).attr("userChoices"));
-                console.log(userSelection);
-                //userSelection = right/wrong
-                //(userSelection === chosenOne.choices[chosenOne.rightChoice]) <--try this
-                //(userSelection === chosenOne.rightChoice)
-                if (userSelection === chosenOne.rightChoice) {
-                    console.log(chosenOne.choices[chosenOne.rightChoice]);
-                    stop();
-                    //clearbck();
-                    selected = true;
-                    rightAnswer++;
-                    userSelection = "";
-                    $(".newBackground").empty();
-                    $("#choicesDiv").html("<p>Damn, boi 🐱‍🐉👌</p>");
-                    hideimage();
-                    console.log(rightAnswer);
-                } else {
-                    stop();
-                    //clearbck();
-                    selected = true;
-                    $(".newBackground").empty();
-                    wrongAnswer++;
-                    userSelection = "";
-                    $("#choicesDiv").html("<p>🤔That is incorrect! The correct choice is: " + chosenOne.choices[chosenOne.rightChoice] + "</p>");
-                    hideimage();
-                    console.log(wrongAnswer);
-                }
-            })
-        }
-        //function clearbck() {
-        //     $(".newBackground").empty();
-        // }
-        function hideimage() {
-            $("#choicesDiv").append("<img src=" + chosenOne.image + ">");
-            newArray.push(chosenOne);
-            trivia.splice(triviaRand, 1);
-
-            var hideimg = setTimeout(function () {
-                $("#choicesDiv").empty();
-                time = 30;
-
-                //run the score screen if all questions answered
-                if ((wrongAnswer + rightAnswer + unansweredCount) === totalCount) {
-                    //clearbck();
-                    $("#questionDiv").empty();
-                    $("#questionDiv").html("<h3>🧐 Game Over!  Let's see your score 😱: </h3>");
-                    $("#choicesDiv").append("<h4> 🤪 Correct: " + rightAnswer + "</h4>");
-                    $("#choicesDiv").append("<h4> 🤬 Incorrect: " + wrongAnswer + "</h4>");
-                    $("#choicesDiv").append("<h4> 🤯 Unanswered: " + unansweredCount + "</h4>");
-                    $("#resetBtn").show();
-                    rightAnswer = 0;
-                    wrongAnswer = 0;
-                    unansweredCount = 0;
-
-                } else {
-                    runTime();
-                    displayTrivia();
-
-                }
-            }, 2000);
-
 
         }
+        //try prepend to add a div containing background img tag
+        //and add background property into object array for each question
+        //use css & z-index to position behind actual container	
+        //$(".container").prepend(chosenOne.background);
+        //console.log(chosenOne.background);
 
-        $("#resetBtn").on("click", function () {
-            $(this).hide();
-            //clearbck();
-            $("#choicesDiv").empty();
-            $("#questionDiv").empty();
-            for (var i = 0; i < placeHolder.length; i++) {
-                trivia.push(placeHolder[i]);
+        //click function to select rightChoice
+        $(".answerChoices").click(function () {
+            //parseInt() function parses a string argument and returns an integer of the specified radix
+            //locate array based on userChoice
+            userSelection = parseInt($(this).attr("userChoices"));
+            console.log(userSelection);
+            //userSelection = right/wrong
+            //(userSelection === chosenOne.choices[chosenOne.rightChoice]) <--try this
+            //(userSelection === chosenOne.rightChoice)
+            if (userSelection === chosenOne.rightChoice) {
+                stop();
+                rightAnswer++;
+                userSelection = "";
+                $("#choicesDiv").html("<p>Damn, boi 🐱‍🐉👌</p>");
+                hideImage();
+                console.log(rightAnswer);
+
+            } else {
+                stop();
+                wrongAnswer++;
+                userSelection = "";
+                $("#choicesDiv").html("<p>🤔That is incorrect! The correct choice is: " + chosenOne.choices[chosenOne.rightChoice] + "</p>");
+                hideImage();
+                console.log(wrongAnswer);
             }
-            runTime();
-            displayTrivia();
-
         })
+    }
+
+
+    function hideImage() {
+        $("#choicesDiv").append("<img src=" + chosenOne.image + ">");
+        newArray.push(chosenOne);
+        trivia.splice(triviaRand, 1);
+
+        var hideImg = setTimeout(function () {
+            $("#choicesDiv").empty();
+            time = 15;
+
+            //run the score screen if all questions looped through & answered
+            if ((wrongAnswer + rightAnswer + unansweredCount) === totalCount) {
+                $("#questionDiv").empty();
+                $("#questionDiv").html("<h3>🧐 Game Over!  Let's see your score 😱: </h3>");
+                $("#choicesDiv").append("<h4> 🤪 Correct: " + rightAnswer + "</h4>");
+                $("#choicesDiv").append("<h4> 🤬 Incorrect: " + wrongAnswer + "</h4>");
+                $("#choicesDiv").append("<h4> 🤯 Unanswered: " + unansweredCount + "</h4>");
+                $("#resetBtn").show();
+                rightAnswer = 0;
+                wrongAnswer = 0;
+                unansweredCount = 0;
+
+            } else {
+                runTime();
+                displayTrivia();
+
+            }
+        }, 5000);
+
 
     }
-})
 
+    $("#resetBtn").click(function () {
+        $(this).hide();
+        $("#choicesDiv").empty();
+        $("#questionDiv").empty();
+        for (var i = 0; i < placeHolder.length; i++) {
+            trivia.push(placeHolder[i]);
+        }
+        runTime();
+        displayTrivia();
+
+    })
+
+})
